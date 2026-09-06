6750573da79cd7402a2bb3b60499d2428c0d3f40
Cuberootex <68776756+Cuberootex@users.noreply.github.com>
2026-09-06 18:00:23 +0200

    Fix case in long_battle_command_prefixes (#1524)

--- lib/teiserver/protocols/spring/spring_in.ex
+++ lib/teiserver/protocols/spring/spring_in.ex
@@ -52,7 +52,7 @@ defmodule Teiserver.Protocols.SpringIn do
   # key=value pairs) and so get a 1024-char allowance instead of the default 256.
   @long_battle_command_prefixes [
     "$welcome-message",
-    "!aiProfile",
+    "!aiprofile",
     "!welcome-message",
     "!mode ",
     "!bset mapmetadata"
